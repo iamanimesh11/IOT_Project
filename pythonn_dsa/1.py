5c33a133bb315a53750c23c49203d9939dffcd27
Animesh Singh <iamanimesh11june@gmail.com>
2025-05-21 20:52:50 +0530

    m

--- pythonn_dsa/1.py
+++ pythonn_dsa/1.py
@@ -9,3 +9,40 @@ for i in string_lists:
         
 
 print(dict)
+
+nums=[1,2,3,4]
+prefix=1
+res=[1,1,1,1]
+for i in range(len(nums)):
+    res[i]=prefix
+    prefix *= nums[i]
+
+print(res)
+postfix=1
+for i in reversed(range(len(nums))):
+    print(i)
+    res[i]=postfix
+    postfix*=nums[i]
+print(res)
+
+
+def productexceptSelf(nums):
+    res=[1]*len(nums)
+    n=len(nums)
+    prefix=1
+    for i in range(len(res)):
+        res[i]=prefix
+        prefix*=nums[i]
+        
+    postfix=1
+    for i in range(n-1,-1,-1):
+        res[i]*=postfix
+        postfix*=nums[i]
+    
+    return res
+
+print(productexceptSelf([1,2,3,4]))
+    
+    
+sub="Abcd"
+print(sub[::-1])
